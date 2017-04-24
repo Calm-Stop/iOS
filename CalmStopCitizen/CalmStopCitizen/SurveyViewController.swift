a91774d5d6b70ce93b0805de9e4b1f00216eeca5
Annisa <annisakaraca@gmail.com>
2017-04-24 15:25:17 -0700

    Calculate new rating for officer using input and firebase data.
    Read avg_rating and number_of_ratings from the database for a currently
    hardcoded officer. Calculate new average rating taking the user input into
    account.

--- CalmStopCitizen/CalmStopCitizen/SurveyViewController.swift
+++ CalmStopCitizen/CalmStopCitizen/SurveyViewController.swift
@@ -7,7 +7,7 @@
 //
 
 import UIKit
-// import Firebase
+import Firebase
 
 class SurveyViewController: UIViewController {
 
@@ -20,6 +20,8 @@ class SurveyViewController: UIViewController {
     @IBOutlet weak var noOpinionButton: UIButton!
     @IBOutlet weak var submitFeedbackButton: UIButton!
     
+    let officerUID = "Tl4pCcIjlxTXQgCcoLp4IB4Hzti2"
+    let officerDeptNo = "14567"
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -43,4 +45,55 @@ class SurveyViewController: UIViewController {
         // change state of button that was tapped to selected
         sender.isSelected = true
     }
+    
+    
+    
+    @IBAction func submitFeedbackTapped(_ sender: UIButton) {
+        var oldOfficerRating: Float!
+        var numberOfRatings: Int!
+        var newOfficerRating: Float!
+        
+        // set officer rating on a 1-5 scale (very satisfied = 5, somewhat satisfied = 4, neutral = 3, somewhat dissatisfied = 2, very dissatisfied = 1, no opinion = 0)
+        var officerRating: Int!
+        
+        if verySatisfiedButton.isSelected {
+            officerRating = 5
+        } else if somewhatSatisfiedButton.isSelected {
+            officerRating = 4
+        } else if neutralButton.isSelected {
+            officerRating = 3
+        } else if somewhatDissatisfiedButton.isSelected {
+            officerRating = 2
+        } else if veryDissatisfiedButton.isSelected {
+            officerRating = 1
+        } else if noOpinionButton.isSelected {
+            officerRating = 0
+        }
+        
+        // get officer rating information from firebase
+        FIRDatabase.database().reference().child("officer").child("14567").child("Tl4pCcIjlxTXQgCcoLp4IB4Hzti2").child("ratings").observeSingleEvent(of: .value, with: { (snapshot) in
+            
+            if let dictionary = snapshot.value as? [String: AnyObject]{
+                oldOfficerRating = dictionary["avg_rating"] as? Float
+                numberOfRatings = dictionary["number_of_ratings"] as? Int
+                print (oldOfficerRating)
+                print (numberOfRatings)
+                
+                // calculate new rating for officer if rating > 0
+                let floatNumberOfRatings = Float(numberOfRatings)
+                let floatOfficerRating = Float(officerRating)
+                newOfficerRating = ((oldOfficerRating * floatNumberOfRatings) + floatOfficerRating ) / (floatNumberOfRatings + 1)
+                print (newOfficerRating)
+            }
+            
+            
+            
+        })
+        
+
+        
+    }
+    
 }
+    
+
