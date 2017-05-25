9281676cea7a18f5ce462aac00882fc2888ae1f5
Annisa <annisakaraca@gmail.com>
2017-05-25 16:45:23 -0700

    Let citizen send all documents on initial contact screen.
    Citizen can send all 3 documents if they are all uploaded to Firebase
    Storage. Each document is saved in a separate folder in Storage,
    with the name of the file being the same as the driver's uid. Documents
    take a little while to upload, so need to include a progress spinner.

--- CalmStopCitizen/CalmStopCitizen/InitialContactViewController.swift
+++ CalmStopCitizen/CalmStopCitizen/InitialContactViewController.swift
@@ -21,6 +21,8 @@ class InitialContactViewController: UIViewController {
     
     // Document variables
     var insuranceImage: UIImage!
+    var registrationImage: UIImage!
+    var licenseImage: UIImage!
 
 
     override func viewDidLoad() {
@@ -71,6 +73,10 @@ class InitialContactViewController: UIViewController {
     @IBAction func sendDocuments(_ sender: UIButton) {
         loadInsurance()
         uploadInsurance()
+        loadRegistration()
+        uploadRegistration()
+        loadLicense()
+        uploadLicense()
     }
     
     func loadInsurance() {
@@ -120,6 +126,119 @@ class InitialContactViewController: UIViewController {
         metaData.contentType = "image/png"
         
         tempImageRef.put(UIImagePNGRepresentation(insuranceImage)!, metadata: metaData) { (data, error) in
+            if error == nil {
+                print("Upload successful")
+            }
+            else{
+                print(error)
+            }
+            
+        }
+    }
+    
+    func loadRegistration() {
+        let app = UIApplication.shared.delegate as! AppDelegate
+        let context = app.persistentContainer.viewContext
+        let request = NSFetchRequest<NSFetchRequestResult>(entityName: "Registration")
+        request.returnsObjectsAsFaults = false
+        
+        do {
+            
+            let results = try context.fetch(request)
+            
+            if results.count > 0 {
+                
+                print("Registration Image found!")
+                
+                for result in results as! [NSManagedObject] {
+                    
+                    if let imageData = result.value(forKey: "registrationImage") as? NSData {
+                        if let image = UIImage(data: imageData as Data) {
+                            registrationImage = image
+                        }
+                    }
+                }
+                
+            } else {
+                print("Profile : No data found")
+                registrationImage = nil
+            }
+        } catch {
+            
+            print ("Error Loading")
+        }
+
+    }
+    
+    func uploadRegistration() {
+        // Upload
+        
+        let uid = FIRAuth.auth()?.currentUser?.uid
+        
+        let storage = FIRStorage.storage().reference()
+        
+        let tempImageRef = storage.child("images/documents/registration/" + uid!)
+        
+        let metaData = FIRStorageMetadata()
+        metaData.contentType = "image/png"
+        
+        tempImageRef.put(UIImagePNGRepresentation(registrationImage)!, metadata: metaData) { (data, error) in
+            if error == nil {
+                print("Upload successful")
+            }
+            else{
+                print(error)
+            }
+            
+        }
+    }
+    
+    func loadLicense() {
+        let app = UIApplication.shared.delegate as! AppDelegate
+        let context = app.persistentContainer.viewContext
+        let request = NSFetchRequest<NSFetchRequestResult>(entityName: "License")
+        request.returnsObjectsAsFaults = false
+        
+        do {
+            
+            let results = try context.fetch(request)
+            
+            if results.count > 0 {
+                
+                print("License Image found!")
+                
+                for result in results as! [NSManagedObject] {
+                    
+                    if let imageData = result.value(forKey: "licenseImage") as? NSData {
+                        if let image = UIImage(data: imageData as Data) {
+                            licenseImage = image
+                        }
+                    }
+                }
+                
+            } else {
+                print("Profile : No data found")
+                licenseImage = nil
+            }
+        } catch {
+            
+            print ("Error Loading")
+        }
+    }
+    
+    func uploadLicense() {
+        // Upload
+        
+        let uid = FIRAuth.auth()?.currentUser?.uid
+        
+        let storage = FIRStorage.storage().reference()
+        
+        let tempImageRef = storage.child("images/documents/license/" + uid!)
+        
+        let metaData = FIRStorageMetadata()
+        metaData.contentType = "image/png"
+        
+        tempImageRef.put(UIImagePNGRepresentation(licenseImage)!, metadata: metaData) { (data, error) in
             if error == nil {
                 print("Upload successful")
             }
