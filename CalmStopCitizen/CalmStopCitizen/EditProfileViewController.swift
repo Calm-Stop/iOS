4dbdf98f3d7eedb687e899d2675461a1d03dc18b
Annisa <annisakaraca@gmail.com>
2017-05-28 14:33:27 -0700

    Build front-end for profile editing.

---
+++ CalmStopCitizen/CalmStopCitizen/EditProfileViewController.swift
@@ -0,0 +1,18 @@
+//
+//  EditProfileViewController.swift
+//  CalmStopCitizen
+//
+//  Created by Annisa Karaca on 5/28/17.
+//  Copyright © 2017 Calm Stop. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class EditProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
